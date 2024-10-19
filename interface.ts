c0ec0a82d1b481a7d324f9bb0ccfa72031dcfbe9
HEKYPTO <tsunyanapat@gmail.com>
2024-10-20 05:02:10 +0700

    CHEKCPOINT 1

--- interface.ts
+++ interface.ts
@@ -1,19 +1,27 @@
 interface HospitalItem {
-    _id: string,
-    name: string,
-    address: string,
-    district: string,
-    province: string,
-    postalcode: string,
-    tel: string,
-    picture: string,
-    __v: number,
-    id: string
-  }
+  _id: string,
+  name: string,
+  address: string,
+  district: string,
+  province: string,
+  postalcode: string,
+  tel: string,
+  picture: string,
+  __v: number,
+  id: string
+}
   
-  interface HospitalJson {
-    success: boolean,
-    count: number,
-    pagination: Object,
-    data: HospitalItem[]
-  }
+interface HospitalJson {
+  success: boolean,
+  count: number,
+  pagination: Object,
+  data: HospitalItem[]
+}
+
+interface BookingItem {
+  name: string
+  surname: string
+  id: string
+  hospitial: string
+  bookDate: string
+}
